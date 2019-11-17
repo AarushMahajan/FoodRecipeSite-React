1d77bac7b858bd2a04e6bef746032187c3db154f
AarushMahajan <aarushmahajan15@gmail.com>
2019-11-17 11:28:47 +0530

    Updates

---
+++ precache-manifest.8b1735b75c1641e8eea2d6860cf72e7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2480ed5d45d999bc07089b6b1892323",
+    "url": "/FoodRecipeSite-React/index.html"
+  },
+  {
+    "revision": "1a873dadee47cc2a787b",
+    "url": "/FoodRecipeSite-React/static/css/main.3a1c1c67.chunk.css"
+  },
+  {
+    "revision": "1f1a576f294ac939f1f1",
+    "url": "/FoodRecipeSite-React/static/js/2.b4751a17.chunk.js"
+  },
+  {
+    "revision": "1a873dadee47cc2a787b",
+    "url": "/FoodRecipeSite-React/static/js/main.bb7ea908.chunk.js"
+  },
+  {
+    "revision": "b52097d93317ae5afb14",
+    "url": "/FoodRecipeSite-React/static/js/runtime-main.39651e3f.js"
+  }
+]);
